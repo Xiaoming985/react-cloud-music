cf3ac229369091d2f2d7d090e2af061ee69c3e6d
Xiaoming985 <765701782@qq.com>
2021-09-30 20:36:48 +0800

    production

--- src/store/actions/music.js
+++
@@ -1,4 +0,0 @@
-import { PLAY_MUSIC, ADD_MUSIC } from "../constant"
-
-export const playMusic = data => ({type: PLAY_MUSIC, data})
-export const addMusic = data => ({type: ADD_MUSIC, data})
--- src/store/constant.js
+++
@@ -1,2 +0,0 @@
-export const PLAY_MUSIC = "play_music"
-export const ADD_MUSIC = 'add_music'
